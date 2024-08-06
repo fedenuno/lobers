45a8278bf3cb529f7f5c02a35ef6f0a05129949a
Fede <federico.nuno@gmail.com>
2024-08-06 13:25:54 -0600

    Actualización pruebas de envío via whatsapp

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Twilio\Rest\Client;
 
 class HomeController extends Controller
 {
@@ -21,9 +22,38 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index(Request $request)
-    {
+    public function index(Request $request) {
         $request->user()->authorizeRoles(['user', 'admin']);
+        $sid = env('TWILIO_SID');
+        $token = env('TWILIO_AUTH_TOKEN');
+        $number = env('TWILIO_NUMBER');
+        $twilio = new Client($sid, $token);
+
+        /*
+         * Prueba mensaje con variable
+         */
+        /*$message = $twilio->messages->create(
+            "whatsapp:+5213328063089", // to
+            [
+                "contentSid" => "HXd8a15553889ab324865c71917d67acf7",
+                "from" => "MG984ef00c2c32d86d280506bb893d8e48",
+                "contentVariables" => json_encode([
+                    "nombre" => "Fede",
+                ]),
+            ]
+        );*/
+
+        /*
+         * Prueba mensaje multimedia
+         */
+        $message = $twilio->messages->create(
+            "whatsapp:+5213328063089", // to
+            [
+                "contentSid" => "HX271bbddb1baf91fa338b988ab424ad0e",
+                "from" => "MG984ef00c2c32d86d280506bb893d8e48",
+            ]
+        );
+
         return view('home');
     }
 }
